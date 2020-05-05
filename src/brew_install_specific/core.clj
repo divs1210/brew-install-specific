f8028131edc7a08e2d4f302b61d8a09d66d9209b
Divyansh Prakash <divyansh.prakash@yahoo.com>
2020-05-06 01:56:13 +0530

    Working: get versions

--- src/brew_install_specific/core.clj
+++ src/brew_install_specific/core.clj
@@ -1,9 +1,28 @@
 (ns brew-install-specific.core
   (:gen-class)
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.pprint :refer [pprint]]
+            [clj-http.client :as http]
+            [net.cgrand.enlive-html :as html]))
 
-(defonce formula-url
-  "https://github.com/Homebrew/homebrew-core/blob/master/Formula/%s.rb")
+(defonce commits-url*
+  "https://github.com/Homebrew/homebrew-core/commits/master/Formula/%s.rb")
+
+(defn fetch-versions
+  [data version]
+  (let [i (atom 0)]
+    (for [commit-title-element (html/select data [:p.commit-title :a])
+          :let [{:keys [aria-label href]} (:attrs commit-title-element)
+                commit-msg aria-label
+                commit-sha (-> href
+                               (str/replace-first "/Homebrew/homebrew-core/commit/" "")
+                               (str/split #"#diff")
+                               first)]
+          :when (str/includes? commit-msg version)]
+      {:id (swap! i inc)
+       :msg commit-msg
+       :sha commit-sha
+       :url (str "https://github.com/Homebrew/homebrew-core/commit/" commit-sha)})))
 
 (defn -main
   [& [package-at-version]]
@@ -17,7 +36,11 @@
       (if-not (and (seq package)
                    (seq version))
         (println "Usage: brew-install-specific package@version")
-        (do
-          (println "Fetching versions for:" package
-                   "\nFrom:" (format formula-url package))
-          )))))
+        (let [commits-uri (-> commits-url*
+                              (format package)
+                              java.net.URI.)
+              data (html/html-resource commits-uri)
+              versions (fetch-versions data version)]
+          (println "Versions:")
+          (doseq [{:keys [id msg url]} (sort-by :id versions)]
+            (println (str id ". " msg " " url))))))))
