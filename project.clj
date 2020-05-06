b1e4373574d15416833b21344c8c8298cbf31aa4
Divyansh Prakash <divyansh.prakash@yahoo.com>
2020-05-06 12:54:33 +0530

    Add native build task

--- project.clj
+++ project.clj
@@ -8,4 +8,14 @@
                  [clj-jgit "1.0.0-beta3"]]
   :main ^:skip-aot brew-install-specific.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[lein-shell "0.5.0"]]}}
+  :aliases
+  {"native"
+   ["shell"
+    "native-image"
+    "--no-fallback"
+    "--report-unsupported-elements-at-runtime"
+    "--initialize-at-build-time"
+    "-jar" "./target/uberjar/${:uberjar-name:-${:name}-${:version}-standalone.jar}"
+    "-H:Name=./target/${:name}"]})
